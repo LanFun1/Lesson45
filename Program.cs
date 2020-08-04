a69d10727883dcccb7e5bc9d3b4c0f3f2c1dabe3
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-08-04 12:48:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 6, 12, 6, 21, 6, 8, 2, 2, 1};
+            Shuffle(ref array);
+            foreach (int element in array)
+            {
+                Console.Write(element+" ");
+            }
+        }
+
+        static int[] Shuffle(ref int[] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int someNumber = array[i];
+                int position = rand.Next(0, array.Length - 1);
+                array[i] = array[position];
+                array[position] = someNumber;
+            }
+            return array;
+        }
+    }
+}
